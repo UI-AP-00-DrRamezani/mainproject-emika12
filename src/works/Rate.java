4bf1c5185835c64ed706d8212d18017f88f63f4f
melika <melikaahappy@yahoo.com>
2022-06-02 02:22:24 +0430

    adding random classes

---
+++ src/works/Rate.java
@@ -0,0 +1,40 @@
+package works;
+
+import account.making.Buyer;
+import making.Buyer;
+import products.Product;
+
+import java.io.Serializable;
+
+public class Rate implements Serializable
+{
+    private Buyer buyer;
+    private int rate;
+    private Product product;
+
+    public Buyer getBuyer() {
+        return buyer;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setBuyer(Buyer buyer) {
+        this.buyer = buyer;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
+
+
